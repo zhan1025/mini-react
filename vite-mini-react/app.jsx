b4e05d0ee9704882c35026044d4ad173d8130ab1
zhan1025 <zhan1025@aliyun.com>
2024-01-14 17:04:35 +0800

    mod:熟悉js pragma拓展

--- vite-mini-react/app.jsx
+++ vite-mini-react/app.jsx
@@ -1,4 +1,12 @@
+// js pragma
+
+/** */
+// `@jsx` MyReact.createElement 
 import React from "./core/React.js";
 // const App = React.createElement('div',{id:'app'},'helloword');
 const App = <div>hello world 123</div>
+// function App() {
+//     return <div>123</div>
+// }
+// console.log(App)
 export default App
